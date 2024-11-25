4c899df4b16e142969812fecf5b3e7ca8a2f7fe3
Vũ Quốc Vương <quoc.vuong@blockodyssey.io>
2024-11-26 00:57:49 +0700

    feat: server island architecture

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://v-learn-astro.netlify.app"
+  site: "https://v-learn-astro.netlify.app",
+  integrations: [preact()]
 });
